d2e0f582f0343361af3acde6e27e67970ceb34aa
Nicolás Selman <nicolas.selman.25@gmail.com>
2022-10-20 19:05:41 -0600

    Added routes calculation

--- components/device/Map.tsx
+++ components/device/Map.tsx
@@ -1,8 +1,6 @@
-import React, { FC, useMemo, useEffect, useState, ReactElement } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
-import { GoogleMap, MarkerF, useJsApiLoader, Marker } from '@react-google-maps/api';
-
-import { Typography } from '@mui/material';
+import { GoogleMap, MarkerF, useJsApiLoader, DirectionsRenderer } from '@react-google-maps/api';
 
 import { Container, Coordinates } from '../../interfaces';
 
@@ -14,21 +12,45 @@ const center: Coordinates = {
   lng: -77.04626663309747,
 }
 
+const pointsOfInteres: Coordinates[] = [
+  { lat: -12.082514011004939, lng: -77.05626663309747 },
+  { lat: -12.082514011004939, lng: -77.04626663309747 },
+  { lat: -12.092514011004939, lng: -77.04626663309747 },
+];
+
 interface Props {
   containers: Container[];
 }
 
 export const Map: FC<Props> = ({ containers = [] }) => {
 
+  const [direction, setDirection] = useState<google.maps.DirectionsResult | null>(null);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+    const calculateRoute = async() => {
+  
+      console.log('getting routes')
+      const directionService = new google.maps.DirectionsService();
+  
+      const results = await directionService.route({
+        origin: { lat: containers[0].lat, lng: containers[0].lng },
+        destination: { lat: containers[1].lat, lng: containers[1].lng },
+        travelMode: google.maps.TravelMode.DRIVING,
+      });
+  
+      setDirection(results);
+    }
+
+    calculateRoute();
+  }, [containers]);
+
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY!,
   });
-
-  // If cannot be loaded, display a custom message
-  if (!isLoaded)
-    return <Typography variant='h1' component='h1'>Not loaded</Typography>;
   
-
+  if (!isLoaded) return <></>;
+  
   return (
     <>
       <GoogleMap
@@ -41,6 +63,7 @@ export const Map: FC<Props> = ({ containers = [] }) => {
             <MarkerF position={{lat, lng}} key={ idx } icon={ fillLevel === 'rojo' ? `${ iconsRoute }/red-dot.png` : `${ iconsRoute }/green-dot.png` } />
           ))
         }
+        { direction && <DirectionsRenderer directions={ direction } />}
       </GoogleMap>
     </>
   );
